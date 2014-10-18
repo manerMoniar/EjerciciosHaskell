0c2a92d2257f8bd97ca8b6b3db5f83e614d5af56
Juanjo <juanjo_23@live.com>
2014-10-18 15:42:17 -0500

    18-20
    Ejercicios del 18 al 20

--- ejemplo_proposicion.hs
+++ ejemplo_proposicion.hs
@@ -1,7 +1,7 @@
 logEquiv2 :: (Bool -> Bool -> Bool) -> (Bool -> Bool -> Bool) -> Bool
 logEquiv2 bf1 bf2 = 
 	and [(bf1 p q) == (bf2 p q) | p <- [True,False],
-									  q <- [True,False]]
+								  q <- [True,False]]
 
 logEquiv3 :: (Bool -> Bool -> Bool -> Bool) -> (Bool -> Bool -> Bool -> Bool) -> Bool
 logEquiv3 bf1 bf2 = 
@@ -10,22 +10,34 @@ logEquiv3 bf1 bf2 =
 									  r <- [True,False]]
 
 
+numero15 = logEquiv3 (\p q r -> (p <= q) <= r)
+					 (\ a b c -> a <= ( b <= c))
 
-
---ejemplo1 =  (\ p q r -> not(p &&  q && r))
---		(\ a b c -> (not a) || (not b) || (not c))
-
---numero15 = (\p q r -> (p <= q) <= r)
---			(\ a b c -> a <= ( b <= c)) 
-
---numero16 = (\ p q -> p <= q) (\ a b -> (not b) <= (not a)) 
+numero16 = logEquiv2 (\ p q -> p <= q)
+					 (\ a b -> (not b) <= (not a)) 
  
---numero17 = (\ p q -> (not p) == q) (\ p q -> p == (not q))
+numero17 = logEquiv2 (\ p q -> (not p) == q)
+					 (\ p q -> p == (not q))
 
-main =  do 
-	print (logEquiv3 (\p q r -> (p <= q) <= r) (\ a b c -> a <= ( b <= c)) ) 
-	print (logEquiv2 (\ p q -> p <= q) (\ a b -> (not b) <= (not a)) )
-	print (logEquiv2 (\ p q -> (not p) == q) (\ p q -> p == (not q)) )
-	
-	
-	
+numero18 = logEquiv2 (\ p q -> not(p /= q))
+					 (\ p q -> p == q)
+
+numero19 = logEquiv2 (\ p q -> not(p) == not(q))
+					 (\ p q -> p == q)
+
+numero20 = logEquiv3 (\ p q r -> (p <=q ) && (p <= r))
+					 (\ p q r -> p <= (q && r))
+
+main =  do 	
+	putStr "Ejercicio 15: "
+	print numero15
+	putStr "Ejercicio 16: "
+	print numero16
+	putStr "Ejercicio 17: "
+	print numero17	
+	putStr "Ejercicio 18: "
+	print numero18
+	putStr "Ejercicio 19: "
+	print numero19
+	putStr "Ejercicio 20: "
+	print numero20
